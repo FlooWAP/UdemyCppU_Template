eba6c5ec15f6a5edb976ca835ad18b69a4660cb3
franneck94 <schaffranek.jan@web.de>
2022-09-26 11:43:12 +0200

    up

---
+++ 02_Basics/Datatypes.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int i = 1;
+    char j = 2;
+    unsigned short k = 44;
+    long long l = 2022;
+
+    float m = 23.5F;
+    double n = 33.175;
+
+    return 0;
+}
