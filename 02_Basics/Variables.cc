504289d5ac2eda4ab9ded8428bb75b3e482089f0
FlooWAP <113284666+FlooWAP@users.noreply.github.com>
2023-02-27 11:17:28 +0100

    12.

---
+++ 02_Basics/Variables.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// 1.) Can not start with a number
+// 2.) Can not contain whitespaces
+// 3.) Variable name must be unique
+
+int main()
+{
+    int my_integer_value;
+
+    my_integer_value = 2;
+
+    return 0;
+}
